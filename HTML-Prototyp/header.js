c1399819a217b8a4e2d39ef69c5ebf25c3663e48
Mathieu Wessel <wesselma@hs-albsig.de>
2022-04-03 17:56:47 +0200

    moved everything into one Folder

---
+++ HTML-Prototyp/header.js
@@ -0,0 +1,34 @@
+document.write('\
+<div>\
+<table>\
+    <tr>\
+        <th class="border">\
+            <img id="Logo" src="./lib/logo2.png" align="left">\
+            <h1 class="welcome">Herzlich Wilkommen!</h1>\
+        </th>\
+        <th class="border">\
+            <div>\
+                <input class="search" type="text" placeholder="Suchen">\
+            </div>\
+        </th>\
+        <th class="border">\
+            <img id="Profil" src="./lib/profil.png">\
+        </th>\
+        <th class="border">\
+            <nav>\
+                <div class="dropdown">\
+                    <button class="dropbtn">Menü</button>\
+                    <div class="dropdown-content">\
+                        <a href="#">Link 1</a>\
+                        <a href="#">Link 2</a>\
+                        <a href="#">Link 3</a>\
+                        <a href="#">Link 4</a>\
+                        <a href="#">Link 5</a>\
+                    </div>\
+                </div>\
+            </nav>\
+        </th>\
+    </tr>\
+</table>\
+</div>\
+');
