8eb8cd70c0201fb1e3b7734cdb1d03f0cb9d0554
Mathieu Wessel <wesselma@hs-albsig.de>
2022-04-06 14:31:06 +0200

    profile tabs body js implementation

--- HTML-Prototyp/profile.js
+++ HTML-Prototyp/profile.js
@@ -3,11 +3,49 @@ document.addEventListener("DOMContentLoaded", main);
 function main() {
     console.log("DOMContentLoaded")
     
+    var tab_body = document.getElementById("tab_body");
+
     var profile_info_tab = document.getElementById("profile_info_tab");
     profile_info_tab.addEventListener("click", () => {
         profile_info_tab.className = 'tab_current';
         user_questions_tab.className = 'tab_other';
         user_answers_tab.className = 'tab_other';
+
+        tab_body.innerHTML =  ""
+
+        profile_left = document.createElement("div")
+        tab_body.append(profile_left)
+        profile_left.setAttribute("id", "profile_left")
+        profile_left.setAttribute("class", "profile_left")
+
+        profile_right = document.createElement("div")
+        tab_body.append(profile_right)
+        profile_right.setAttribute("id", "profile_right")
+        profile_right.setAttribute("class", "profile_right")
+
+        p = document.createElement("p")
+        profile_left.append(p)
+        p.innerHTML = "Questions:"
+
+        p = document.createElement("p")
+        profile_left.append(p)
+        p.innerHTML = "Answers:"
+
+        p = document.createElement("p")
+        profile_left.append(p)
+        p.innerHTML = "Member since:"
+
+        p = document.createElement("p")
+        profile_right.append(p)
+        p.innerHTML = "0"
+
+        p = document.createElement("p")
+        profile_right.append(p)
+        p.innerHTML = "0"
+
+        p = document.createElement("p")
+        profile_right.append(p)
+        p.innerHTML = "05.04.2022"
     });
 
     var user_questions_tab = document.getElementById("user_questions_tab")
@@ -15,6 +53,17 @@ function main() {
         profile_info_tab.className = 'tab_other';
         user_questions_tab.className = 'tab_current';
         user_answers_tab.className = 'tab_other';
+
+        tab_body.innerHTML = ""
+
+        profile_qa = document.createElement("div")
+        tab_body.append(profile_qa)
+        profile_qa.setAttribute("id", "profile_qa")
+        profile_qa.setAttribute("class", "profile_qa")
+
+        h2 = document.createElement("h2")
+        profile_qa.append(h2)
+        h2.innerHTML = "User asked:"
     });
 
     var user_answers_tab = document.getElementById("user_answers_tab");
@@ -22,6 +71,17 @@ function main() {
         profile_info_tab.className = "tab_other";
         user_questions_tab.className = "tab_other";
         user_answers_tab.className = "tab_current";
+
+        tab_body.innerHTML = ""
+
+        profile_qa = document.createElement("div")
+        tab_body.append(profile_qa)
+        profile_qa.setAttribute("id", "profile_qa")
+        profile_qa.setAttribute("class", "profile_qa")
+
+        h2 = document.createElement("h2")
+        profile_qa.append(h2)
+        h2.innerHTML = "User answered:"
     });
 
     document.querySelector("user_questions")
