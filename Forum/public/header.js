a402bef08e26995fa601f6930a2976927ea6b986
Jonas <reinecj@hs-albsig.de>
2022-04-22 09:57:59 +0200

    HTML Prototyp auf Webserver eingebunden

---
+++ Forum/public/header.js
@@ -0,0 +1,34 @@
+document.write('\
+<div class="header">\
+<table class ="table_header">\
+    <tr>\
+        <th class="border">\
+            <a href="./index.html"><img id="Logo" src="./lib/logo2.png" align="left" alt="Logo"></a>\
+            <h1 class="welcome">Welcome!</h1>\
+        </th>\
+        <th class="border">\
+            <div>\
+                <input class="search" type="text" placeholder="Search">\
+            </div>\
+        </th>\
+        <th class="border">\
+            <a href="profile.html"><img id="Profil" src="./lib/profil.png" alt="Profil"></a>\
+        </th>\
+        <th class="border">\
+            <nav>\
+                <div class="dropdown">\
+                    <button class="dropbtn">Menu</button>\
+                    <div class="dropdown-content">\
+                    <a href="index.html">Home</a>\
+                    <a href="questionSide.html">Ask Question</a>\
+                    <a href="kategorie.html">Categories</a>\
+                    <a href="login.html">Log in</a>\
+                    <a href="signup.html">Sign up</a>\
+                    </div>\
+                </div>\
+            </nav>\
+        </th>\
+    </tr>\
+</table>\
+</div>\
+');
--- Forum/routes/index.js
+++ Forum/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+const express = require('express');
+const { path } = require('express/lib/application');
 var router = express.Router();
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.use(express.static('public'));
+
+//router.get('/', function(req, res, next) {
+
+  //res.render('index', { title: 'Express' });
+  //res.sendFile(__dirname + '/html/home.html');
+  //res.sendFile(__dirname + '/html/header.js')
+  //res.sendFile(__dirname + '/html/lib/style.css');
+//});
 
 module.exports = router;
