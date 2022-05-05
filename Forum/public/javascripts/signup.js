cd6798bd7503f4a34bd5a05fea04dae79a5f3636
Jonas <reinecj@hs-albsig.de>
2022-05-05 10:21:28 +0200

    Added password and alerts

--- Forum/public/javascripts/signup.js
+++ Forum/public/javascripts/signup.js
@@ -1,17 +1,96 @@
 first_name_input = document.getElementById("first_name");
+password_input = document.getElementById("password");
+password_repeat_input = document.getElementById("repeat_password");
 submitbutton = document.getElementById("submit");
 
 submitbutton.addEventListener("click", sendForm);
+//submitbutton.addEventListener("click", checkFormular);
 
-function sendForm(){
+function sendForm() {
     text = first_name_input.value
-    data = {
-        "id": text
+
+    if (password_input.value === password_repeat_input.value && checkFormular()) {
+        password = password_input.value;
+        data = {
+            "id": text,
+            "password": password,
+        }
+
+        fetch("http://localhost:3000/test", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+    }
+    if (password_input.value != password_repeat_input.value) {
+        window.alert("Passwords are not the same!");
     }
 
-    fetch("http://localhost:3000/test", {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(data)
-    })
+}
+
+function checkFormular() {
+
+    let checkBox = true;
+    let first_name, last_name, email, username, password, repeat_password;
+
+    first_name = document.getElementById("first_name");
+    last_name = document.getElementById("last_name");
+    email = document.getElementById("e-mail");
+    username = document.getElementById("username");
+    password = document.getElementById("password");
+    repeat_password = document.getElementById("repeat_password");
+
+    if (first_name.value == "") {
+        //first_name.style.borderColor = "red";
+        window.alert("First name is empty!");
+        checkBox = false;
+    }
+    else {
+        first_name.style.borderColor = "white";
+    }
+    if (last_name.value.length == 0) {
+        //last_name.style.borderColor = "red";
+        window.alert("Last name is empty!");
+        checkBox = false;
+    }
+    else {
+        last_name.style.borderColor = "white";
+    }
+    if (email.value.length == 0) {
+        //email.style.borderColor = "red";
+        window.alert("E-Mail is empty!");
+        checkBox = false;
+    }
+    else {
+        email.style.borderColor = "white";
+    }
+
+    if (username.value.length == 0) {
+        //username.style.borderColor = "red";
+        window.alert("User name is empty!");
+        checkBox = false;
+    }
+    else {
+        username.style.borderColor = "white";
+    }
+
+    if (password.value.length == 0) {
+        //password.style.borderColor = "red";
+        window.alert("Password is empty!");
+        checkBox = false;
+    }
+    else {
+        password.style.borderColor = "white";
+    }
+
+    if (repeat_password.value.length == 0) {
+        //repeat_password.style.borderColor = "red";
+        window.alert("Repeat Password is empty!");
+        checkBox = false;
+    }
+    else {
+        repeat_password.style.borderColor = "white";
+    }
+    return checkBox;
 }
--- Forum/routes/index.js
+++ Forum/routes/index.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const { path } = require('express/lib/application');
+const bcrypt = require('bcrypt');
 var router = express.Router();
 const app = express();
-var db = require('../database')
+var db = require('../database');
+const { json } = require('express/lib/response');
 
 app.use(express.static('public'));
 
 //router.get('/', function(req, res, next) {
 
-  //res.render('index', { title: 'Express' });
-  //res.sendFile(__dirname + '/html/home.html');
-  //res.sendFile(__dirname + '/html/header.js')
-  //res.sendFile(__dirname + '/html/lib/style.css');
+//res.render('index', { title: 'Express' });
+//res.sendFile(__dirname + '/html/home.html');
+//res.sendFile(__dirname + '/html/header.js')
+//res.sendFile(__dirname + '/html/lib/style.css');
 //});
 
 router.post("/test", (req, res) => {
+
   let jsonData = req.body;
-  var sql = "insert into test VALUES ('" + jsonData.id + "', '" + jsonData.id + "');"
+  /*
+  bcrypt.genSalt(2, function (err, salt) {
+    bcrypt.hash(jsonData.password, salt, function (err, hash) {
+      console.log(typeof (hash));
+    });
+  });
+  */
+  var sql = "insert into test (id, password) VALUES ('" + jsonData.id + "', '" + jsonData.password + "');"
   console.log(sql);
   var params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
-      res.status(400).json({"error":err.message});
+      res.status(400).json({ "error": err.message });
       return;
     }
     res.send(200);
