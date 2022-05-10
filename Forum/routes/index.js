279528217c3af5fc4b4fdf9da4109cfbc9e0745f
Mathieu Wessel <wesselma@hs-albsig.de>
2022-05-10 08:13:17 +0200

    bcrypt functional

--- Forum/routes/index.js
+++ Forum/routes/index.js
@@ -6,6 +6,7 @@ var router = express.Router();
 const app = express();
 var db = require('../database');
 const { json } = require('express/lib/response');
+const { hash } = require('bcrypt');
 
 app.use(express.static('public'));
 
@@ -17,19 +18,16 @@ app.use(express.static('public'));
 //res.sendFile(__dirname + '/html/lib/style.css');
 //});
 
-router.post("/new_user", (req, res) => {
+router.post("/new_user", async (req, res) => {
   let jsonData = req.body;
   let new_id = nanoid();
   let new_join_date = new Date().toISOString().slice(0, 10);
   let profilepicturepath = "/default";
-  /*
-  bcrypt.genSalt(2, function (err, salt) {
-    bcrypt.hash(jsonData.password, salt, function (err, hash) {
-      console.log(typeof (hash));
-    });
-  });
-  */
-  var sql = "insert into users (id, firstname, lastname, email, username, passwordhash, joindate, profilepicturepath ) VALUES ('" + new_id + "', '" + jsonData.first_name + "', '" + jsonData.last_name + "','" + jsonData.email + "','" + jsonData.username + "','" + jsonData.password + "','" + new_join_date + "','" + profilepicturepath + "');"
+  
+  const salt = await bcrypt.genSalt(2);
+  const hashed_password = await bcrypt.hash(jsonData.password, salt);
+  
+  var sql = "insert into users (id, firstname, lastname, email, username, passwordhash, joindate, profilepicturepath ) VALUES ('" + new_id + "', '" + jsonData.first_name + "', '" + jsonData.last_name + "','" + jsonData.email + "','" + jsonData.username + "','" + hashed_password + "','" + new_join_date + "','" + profilepicturepath + "');"
   var params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
